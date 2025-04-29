edfe0627c051ba29ca41b1a75a3dfef60f5841d0
Mikelemag <mikelepinto2017@gmail.com>
2025-04-29 11:47:07 -0300

    adicionamos if e return no projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,142 @@
+import { useState } from "react";
 import CardFront from "./components/FrontCard";
+import { ToastContainer, toast } from 'react-toastify';
+import BackCard from "./components/BackCard";
+import instance from "./api/instance";
 
 export default function App(){
-  return(
+  const [nome, setNome] = useState("");
+  const [numero, setNumero] = useState("");
+  const [mes, setMes] = useState(0);
+  const [ano, setAno] = useState(0);
+  const [cvv, setCvv] = useState(0);
+  const [senha, setSenha] = useState("");
+
+  async function pagar(){
+    if(!nome || !numero || !mes || !ano || !cvv || !senha){
+      return toast.error("Preencha todos os campos")
+    }
+    if(numero.length !== 16){
+      return toast.error("Número do cartão inválido")
+    }
+    if(cvv.length !== 3){
+      return toast.error("Cvv inválido")
+    }
+    if(ano.length !==2){
+      return toast.error ("Ano de expiração inválida")
+    }
+    if(mes > 12 || mes < 1){
+      return toast.error("Data de expiração inválida")
+    }
+
+    if(senha.length < 4){
+      return toast.error("Senha inválida")
+    }
+    try {
+      
+      const response = await instance.post("/creditcards",{
+      name: nome,
+      number: numero,
+      expiration: `${mes}/{ano}`,
+      cvv: cvv,
+      password: senha
+    })
+
+    return toast.success ("Pagamento realizado com sucesso")
+    } catch (error) {
+      return toast.error ("Erro ao processsar o pagamento")
+      
+    }
+
+  }
+
+  return (
     <div className="w-full h-screen flex">
+      <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      theme="colored"
+      />
       <div className="w-[40%] relative h-full bg-[#271540]">
-        <div className="absolute top-10 left-40">
-        <CardFront />
+        <div className="absolute top-10 left-60">
+          <CardFront />
+        </div>
+        <div className="absolute top-[400px] left-[350px]">
+          <BackCard />
         </div>
       </div>
-      <div className="w-[60%] h-full flex justify-end p-[40px]">
-        <h1 className="text-[45px] w-[70%] h-[150px] font-bold">Preencha os campos para concluir o pagamento</h1>
+      <div className="w-[60%] h-full flex items-end p-[40px] flex-col">
+        <h1 className="text-[40px] w-[60%] h-[150px] font-bold">
+          Preencha os campos para concluir o pagamento
+        </h1>
+        <div className="w-[65%] h-auto min-h-[200px] flex flex-col gap-4">
+          <div className="w-full flex flex-col">
+            <label htmlFor="nome" className="text-[20px]">
+              Nome no cartão
+            </label>
+            <input
+              onChange={(event) => setNome(event.target.value)}
+              type="text"
+              className="w-full h-[40px] rounded-md bg-[#D9D9D9]"
+            />
+          </div>
+          <div className="w-full flex flex-col">
+            <label htmlFor="numero" className="text-[20px]">
+              Número do cartão
+            </label>
+            <input
+              onChange={(event) => setNumero(event.target.value)}
+              type="text"
+              className="w-full h-[40px] rounded-md bg-[#D9D9D9]"
+            />
+          </div>
+          <div className="flex">
+            <div className="w-[70%] flex flex-col">
+              <label htmlFor="" className="text-[20px]">
+                Data de expiração
+              </label>
+              <div className="w-full flex gap-2">
+                <input
+                  type="number"
+                  onChange={(event) => setMes(event.target.value)}
+                  placeholder="MM"
+                  className="w-[50%] pl-2 h-[40px] rounded-md bg-[#D9D9D9]"
+                />
+                <input
+                  type="number"
+                  onChange={(event) => setAno(event.target.value)}
+                  placeholder="AA"
+                  className="w-[50%] pl-2 h-[40px] rounded-md bg-[#D9D9D9]"
+                />
+              </div>
+            </div>
+            <div className="w-[30%] pl-2 flex flex-col">
+              <label htmlFor="" className="text-[20px]">
+                CVV
+              </label>
+              <input
+                type="number"
+                onChange={(event) => setCvv(event.target.value)}
+                className="w-full h-[40px] rounded-md bg-[#D9D9D9] pl-2"
+              />
+            </div>
+          </div>
+          <div className="w-full flex flex-col">
+            <label htmlFor="" className="text-[20px]">
+              Senha do cartão
+            </label>
+            <input
+              type="password"
+              onChange={(event) => setSenha(event.target.value)}
+              className="w-full h-[40px] rounded-md pl-2 bg-[#D9D9D9]"
+            />
+          </div>
+          <button
+            onClick={pagar}
+            className="w-full h-[50px] rounded-md bg-[#271540]
+            text-white font-bold">PAGAR</button>
+        </div> 
       </div>
     </div>
-  )
+  );
 }
